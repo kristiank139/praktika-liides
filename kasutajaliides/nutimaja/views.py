2f5b777463cae64ccad0d9b3ff6dba59e320cdb1
kristiank139 <105385905+kristiank139@users.noreply.github.com>
2023-06-09 15:09:27 +0300

    Lõpetatud

--- kasutajaliides/nutimaja/views.py
+++ kasutajaliides/nutimaja/views.py
@@ -11,34 +11,6 @@ import socket
 import socket
 import subprocess
 
-# Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
-# all interfaces)
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('0.0.0.0', 8000))
-server_socket.listen(0)
-
-# Accept a single connection and make a file-like object out of it
-connection = server_socket.accept()[0].makefile('rb')
-try:
-    # Run a viewer with an appropriate command line. Uncomment the mplayer
-    # version if you would prefer to use mplayer instead of VLC
-    cmdline = ['vlc', '--demux', 'h264', '-']
-    #cmdline = ['mplayer', '-fps', '25', '-cache', '1024', '-']
-    player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
-    while True:
-        # Repeatedly read 1k of data from the connection and write it to
-        # the media player's stdin
-        data = connection.read(1024)
-        if not data:
-            break
-        player.stdin.write(data)
-finally:
-    connection.close()
-    server_socket.close()
-    player.terminate()
-
-
-
 
 HOST = '192.168.75.103' # Siia tuleb nüüd serveri IP-aadress. Kui server ja klient jooksevad samal masinal, saab kasutada praegust IP-aadressit.
 PORT = 65432 # Port peab olema sama nagu serveril.
